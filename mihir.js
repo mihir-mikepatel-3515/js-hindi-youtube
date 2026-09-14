a95a2a095748175ada7d8f9851a91438c1a2d60a
MihirPatel <mihirpatel@MihirPatels-MacBook-Air.local>
2026-09-14 15:08:16 +0530

    new consollle

--- mihir.js
+++ mihir.js
@@ -2,3 +2,6 @@ console.log("Hello, Mihir!");
 console.log("Welcome to the JavaScript world!");
 console.log("Let's explore some coding together!");
 console.log("Keep learning and have fun!");
+console.log("Happy coding, Mihir!");
+console.log("Remember, practice makes perfect!");
+
